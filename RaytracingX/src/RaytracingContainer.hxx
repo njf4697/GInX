5c13d7fdb1a372fb31e9702e179fa889791e2e3b
njf4697 <njf4697@rit.edu>
2026-05-04 16:53:50 -0400

    move printing particle on deletion outside of evolve, just before redistribute

--- RaytracingX/src/RaytracingContainer.hxx
+++ RaytracingX/src/RaytracingContainer.hxx
@@ -527,8 +527,7 @@ namespace RaytracingX
                                 const CCTK_REAL (&x)[10], const CCTK_REAL (&y)[10],
                                 const CCTK_REAL (&z)[10],
                                 const CCTK_REAL (&radius)[10],
-                                const CCTK_REAL (&a)[10],
-                                bool output_final_data, std::string final_data_file_name) //RaytracingX: Add output data.
+                                const CCTK_REAL (&a)[10]) //RaytracingX: Add output data.
         {
 
             if (!zones)

a0be88463fb63a27f88c6c66d1816798ecb89e2b
njf4697 <njf4697@rit.edu>
2026-05-06 12:09:50 -0400

    move horizon check out of evolve

--- RaytracingX/src/RaytracingX.h
+++ RaytracingX/src/RaytracingX.h
@@ -8,7 +8,7 @@
 #ifndef RAYTRACINGX
 #define RAYTRACINGX
 
-#define DEBUG(X) fprintf(stderr, X + std::endl);
+#define DEBUG(X) fprintf(stderr, std::string(X) + "\n");
 
 struct Metric { //struct that contains information about the metric interpolated at a point
     CCTK_REAL alpha;

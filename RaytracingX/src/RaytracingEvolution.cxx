bdb17a97009b59302782d2852759eec49db391f8
njf4697 <njf4697@rit.edu>
2026-05-07 17:50:31 -0400

    flip sign on energy

--- RaytracingX/src/RaytracingEvolution.cxx
+++ RaytracingX/src/RaytracingEvolution.cxx
@@ -113,9 +113,9 @@ extern "C" void R_ParticlesContainer_evolve(CCTK_ARGUMENTS)
 
   Metric m;
   interpolateMetricAtPoint2(CCTK_PASS_CTOC, &m, -1.853942, 15.130371, -0.154485);
-  if (CCTK_MyProc(cctkGH) == 0) { fprintf(stderr, (m.to_string() + "interp. at (%f, %f, %f)").c_str(), -1.853942, 15.130371, -0.154485); }
+  if (CCTK_MyProc(cctkGH) == 0) { fprintf(stderr, (m.to_string() + "interp. at (%f, %f, %f)\n").c_str(), -1.853942, 15.130371, -0.154485); }
   interpolateMetricAtPoint2(CCTK_PASS_CTOC, &m, -32.010824, -31.163003, 16.099812);
-  if (CCTK_MyProc(cctkGH) == 0) { fprintf(stderr, (m.to_string() + "interp. at (%f, %f, %f)").c_str(), -32.010824, -31.163003, 16.099812); }
+  if (CCTK_MyProc(cctkGH) == 0) { fprintf(stderr, (m.to_string() + "interp. at (%f, %f, %f)\n").c_str(), -32.010824, -31.163003, 16.099812); }
 
   //RaytracingX: Add debug print statement.
   if (verbose)

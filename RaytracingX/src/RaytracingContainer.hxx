a55a766b818e189303c579fcb00f456a705938a9
njf4697 <njf4697@rit.edu>
2026-05-07 15:38:06 -0400

    flip sign on energy

--- RaytracingX/src/RaytracingContainer.hxx
+++ RaytracingX/src/RaytracingContainer.hxx
@@ -577,9 +577,10 @@ namespace RaytracingX
       }
 
       if (index[i] == 5029) { DEBUG("test8") }
-
+      
       // f3 = rhs(u + 0.5 * dt * f2, t) for the runge kutta 4 step
       ASSERT_BOUNDS(U_tmp[0], U_tmp[1], U_tmp[2], "k3");
+      if (index[i] == 5029) { DEBUG("test8.5") }
       k_odd = self->compute_rhs(index[i], U_tmp, 0.5 * dt, lapse_array, shift_array,
                                 metric_array, curv_array, rho_array, dt, dx, lev, plo0); //RaytracingX: Add optical depth.
 

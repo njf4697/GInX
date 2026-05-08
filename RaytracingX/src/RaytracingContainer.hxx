2025a8926fa027c298b65495b8e23003b5d8fed0
njf4697 <njf4697@rit.edu>
2026-05-07 21:21:15 -0400

    clamp values for interpolation

--- RaytracingX/src/RaytracingContainer.hxx
+++ RaytracingX/src/RaytracingContainer.hxx
@@ -609,7 +609,7 @@ namespace RaytracingX
 
       // f4 = rhs(u + dt * f3, t) for the runge kutta 4 step
       ASSERT_BOUNDS(U_tmp[0], U_tmp[1], U_tmp[2], "k4");
-      if (index[i] == 4771) { fprintf(stderr, "4771 at %f %f %f\n", U_tmp[0], U_tmp[1], U_tmp[2]); }
+      if (index[i] == 4771) { fprintf(stderr, "(%f %f %f) should be between (%f %f %f) and (%f %f %f)s\n", U_tmp[0], U_tmp[1], U_tmp[2], plo0[0], plo0[1], plo0[2], phi0[0], phi0[1], phi0[2], U_tmp[0], U_tmp[1], U_tmp[2]); }
       k_even = self->compute_rhs(index[i], U_tmp, dt, lapse_array, shift_array,
                                  metric_array, curv_array, rho_array, dt, dx, lev, plo0, phi0); //RaytracingX: Add optical depth.
 

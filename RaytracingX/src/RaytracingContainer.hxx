ab0e65dccdb886cadf231730ef412d3ea90482c5
njf4697 <njf4697@rit.edu>
2026-05-19 18:12:47 -0400

    fix velocity normalization in compute_rhs

--- RaytracingX/src/RaytracingContainer.hxx
+++ RaytracingX/src/RaytracingContainer.hxx
@@ -349,7 +349,7 @@ namespace RaytracingX
             ASSERT_FINITE(v)
             ASSERT_FINITE(this->mass)
 
-            if (amrex::ParallelDescriptor::MyProc == 13 && index == 12053) { fprintf(stderr, "%f %f %f %f\n", this->mass, std::exp(u[3 + StructType::ln_alphaE]), v, this->mass * this->mass / (2.0*std::exp(u[3 + StructType::ln_alphaE]))); }
+            if (amrex::ParallelDescriptor::MyProc() == 13 && index == 12053) { fprintf(stderr, "%f %f %f %f\n", this->mass, std::exp(u[3 + StructType::ln_alphaE]), v, this->mass * this->mass / (2.0*std::exp(u[3 + StructType::ln_alphaE]))); }
 
             const CCTK_REAL alpha_over_v = std::sqrt(1. - this->mass * this->mass / (2*std::exp(u[3 + StructType::ln_alphaE]))) / v;
 

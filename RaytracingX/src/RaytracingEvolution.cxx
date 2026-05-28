f96af4dc9f028a1b7ab54c3c6dcb881fc72bcdd8
njf4697 <njf4697@rit.edu>
2026-05-28 16:28:11 -0400

    test changing metric

--- RaytracingX/src/RaytracingEvolution.cxx
+++ RaytracingX/src/RaytracingEvolution.cxx
@@ -35,6 +35,8 @@
 
 #include "AST_Spacetime_SetMetric.hxx"
 
+#define H5Dopen2 H5Dopen
+
 //RaytracingX: Uses override for particle data struct and particle container defined in RaytracingX/RaytracingContainer.hxx.
 using ParticleData = RaytracingX::RaytracingPhotonsData;
 using PC = RaytracingX::RaytracingParticlesContainer<ParticleData>;

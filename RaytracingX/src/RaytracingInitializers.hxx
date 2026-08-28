a1f7edc2a5bcca92b26a66b9c1e54b18f07f77c3
njf4697 <njf4697@rit.edu>
2026-08-28 17:19:18 -0400

    clean code

--- RaytracingX/src/RaytracingInitializers.hxx
+++ RaytracingX/src/RaytracingInitializers.hxx
@@ -174,6 +174,8 @@ void camera_initializer(ParticleContainerClass &pc, const CCTK_REAL *real_params
       chi[2] = C * e0[2] - e1[2] + b_adj * e2[2] - a_adj * e3[2];
       chi[3] = C * e0[3] - e1[3] + b_adj * e2[3] - a_adj * e3[3];
 
+      ASSERT_FINITE1(chi, 4)
+
       CCTK_REAL chi_lower[4];
       vectorToOneFormArr(chi_lower, chi, real_params);
 

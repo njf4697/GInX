4c23963ddbffda7f9c7fd997c19cbee0815d865f
njf4697 <njf4697@rit.edu>
2026-08-28 17:50:12 -0400

    clean code

--- RaytracingX/src/RaytracingInitializers.hxx
+++ RaytracingX/src/RaytracingInitializers.hxx
@@ -202,12 +202,14 @@ void camera_initializer(ParticleContainerClass &pc, const CCTK_REAL *real_params
       if (calculate_kerr_conserved_quantities) {
         CCTK_REAL shift_raised[3];
         oneformToVectorSpatial(shift_raised, shift_lower, real_params);
-        const CCTK_REAL V_lower[3] = {arrdata[StructType::vx][local_particle_id], arrdata[StructType::vy][local_particle_id], arrdata[StructType::vz][local_particle_id]};
+        const CCTK_REAL V_down[3] = {arrdata[StructType::vx][local_particle_id], arrdata[StructType::vy][local_particle_id], arrdata[StructType::vz][local_particle_id]};
 
-        arrdata[StructType::U0][local_particle_id] = (lapse - (shift_raised[0]*V_lower[0] + shift_raised[1]*V_lower[1] + shift_raised[2]*V_lower[2]));
-        arrdata[StructType::U1][local_particle_id] = (camera_pos[0]*V_lower[1] - camera_pos[1]*V_lower[0]);
-        arrdata[StructType::U2][local_particle_id] = spatialInnerProductArr(V_lower, real_params);
+        arrdata[StructType::U0][local_particle_id] = (lapse - (shift_raised[0]*V_down[0] + shift_raised[1]*V_down[1] + shift_raised[2]*V_down[2]));
+        arrdata[StructType::U1][local_particle_id] = (camera_pos[0]*V_down[1] - camera_pos[1]*V_down[0]);
+        arrdata[StructType::U2][local_particle_id] = spatialInnerProductArr(V_down, real_params);
       }
+
+      ASSERT_FINITE1(V_down)
 
     }
     current_tile++;

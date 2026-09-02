03b0b801374341569ea3dc724f6eb9dc98f9bd54
njf4697 <njf4697@rit.edu>
2026-09-02 00:43:05 -0400

    start trying to enforce particle bounds better

--- RaytracingX/src/RaytracingEvolution.cxx
+++ RaytracingX/src/RaytracingEvolution.cxx
@@ -71,7 +71,11 @@ extern "C" void R_ParticlesContainer_setup(CCTK_ARGUMENTS)
     3,
     gh,
     "ADMBaseX::alp");
-  assert(MIN(MIN(gh[0], gh[1]), gh[2]) >= 5 && "RaytracingX requires ghost zone size of 5");
+  assert(MIN(MIN(gh[0], gh[1]), gh[2]) >= 3 && "RaytracingX requires ghost zone size of 3");
+  
+  Metric m;
+  interpolateMetricAtPoint(m, 0.062791, 0.000061, -13.703892);
+  CCTK_INFO(m.to_string())
 
   //RaytracingX: Particle skip override moved to schedule.ccl
 
